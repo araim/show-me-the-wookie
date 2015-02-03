9d1720fb2493198964931d308d458d67506ede11
araim <git@araim.net>
2015-02-03 20:51:12 +0100

    Solution scanner refactor - extract till you drop

--- SolutionScanner/BuiltinSolutionScanner/SolutionScanner.cs
+++ SolutionScanner/BuiltinSolutionScanner/SolutionScanner.cs
@@ -11,6 +11,8 @@ namespace SolutionDependencyScanner
     {
         private const string ProjectRegexText = "^Project\\(\"{[A-Z0-9-]+}\"\\)\\s*=\\s*\"([^\"]+)\"\\s*,\\s*\"([^\"]+)\"\\s*,\\s*\"{([A-Z0-9-]+)}\"\\s*$";
         private readonly Regex ProjectRegex = new Regex(ProjectRegexText, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+    
         public SolutionScanner()
         {
             
@@ -21,29 +23,41 @@ namespace SolutionDependencyScanner
             SolutionScaffold s = new SolutionScaffold();
             s.FullPath = f.FullName;
             s.Name = Path.GetFileNameWithoutExtension(f.FullName);
+            ScanFileAndAddProjects(f, s);
+            return s;
+        }
+
+        private void ScanFileAndAddProjects(System.IO.FileInfo f, SolutionScaffold s)
+        {
+
             using (var stream = f.OpenText())
             {
                 while (!stream.EndOfStream)
                 {
-                    string line = stream.ReadLine();
-                    if (line.StartsWith("Project("))
-                    {
-                        Match m = ProjectRegex.Match(line);
-                        if (m.Success)
-                        {
-                            ProjectReference pr = new ProjectReference();
-                            pr.ID = m.Groups[3].Value;
-                            pr.Name = m.Groups[1].Value;
-                            pr.Path = Path.Combine(Path.GetDirectoryName(f.FullName), m.Groups[2].Value);
-                            if (pr.Path.EndsWith("proj")) //vsproj, vcproj, etc.
-                            {
-                                s.Projects.Add(pr);
-                            }
-                        }
-                    }
+                    ParseLineAndAddProjectIfFound(f, s, stream.ReadLine());
                 }
             }
-            return s;
+        }
+
+        private void ParseLineAndAddProjectIfFound(System.IO.FileInfo f, SolutionScaffold s, string line)
+        {
+            if (line.StartsWith("Project("))
+            {
+                Match m = ProjectRegex.Match(line);
+                if (m.Success && m.Groups[2].Value.EndsWith("proj")) //vsproj, vcproj, etc.
+                {
+                    CreateAndAddProjectReference(f, s, m);
+                }
+            }
+        }
+
+        private static void CreateAndAddProjectReference(System.IO.FileInfo f, SolutionScaffold s, Match m)
+        {
+            ProjectReference pr = new ProjectReference();
+            pr.ID = m.Groups[3].Value;
+            pr.Name = m.Groups[1].Value;
+            pr.Path = Path.Combine(Path.GetDirectoryName(f.FullName), m.Groups[2].Value);
+            s.Projects.Add(pr);
         }
     }
 }
