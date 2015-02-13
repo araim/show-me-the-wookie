ae6157b5034ab2d2d90187a93071d88dd534e097
araim <git@araim.net>
2015-02-13 16:26:14 +0100

    Graph settings decorator added for later use in settings

---
+++ DependencyVisualizerGraphX/GraphDecorators/IGraphDecorator.cs
@@ -0,0 +1,19 @@
+﻿using GraphX;
+using GraphX.Logic;
+using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyVisualizerGraphX.GraphDecorators
+{
+    internal interface IGraphDecorator<V, E, G, GL>
+        where G : BidirectionalGraph<V, E>
+        where GL : GXLogicCore<V, E, G>
+        where E : EdgeBase<V>
+        where V : VertexBase
+    {
+        void apply(GL glogic);
+    }
+}
---
+++ DependencyVisualizerGraphX/GraphDecorators/LayoutRouteDecorator.cs
@@ -0,0 +1,32 @@
+﻿using GraphX;
+using GraphX.Logic;
+using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyVisualizerGraphX.GraphDecorators
+{
+    internal class LayoutRouteDecorator<V, E, G, GL> : IGraphDecorator<V, E, G, GL>
+        where G : BidirectionalGraph<V, E>
+        where GL : GXLogicCore<V, E, G>
+        where E : EdgeBase<V>
+        where V : VertexBase
+    {
+
+        private readonly LayoutAlgorithmTypeEnum algorithm;
+
+        public LayoutRouteDecorator(LayoutAlgorithmTypeEnum algorithm)
+        {
+            this.algorithm = algorithm;
+        }
+
+        public void apply(GL glogic)
+        {
+            glogic.DefaultLayoutAlgorithm = algorithm;
+        }
+
+    }
+}
